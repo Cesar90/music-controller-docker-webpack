c55f0e9dc985a1bc3d7a3fd44829a69465f9a94b
Cesar <ccordero.007@gmail.cim>
2024-03-15 09:32:42 -0600

    Handling POST Request

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import main, RoomView
+from .views import main, RoomView, CreateRoomView
 
 urlpatterns = [
-    path('home',RoomView.as_view())
+    path('room',RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
