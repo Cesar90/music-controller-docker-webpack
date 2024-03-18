f93c2316c0d2c0719a635a9c586f094667200ecc
Cesar <ccordero.007@gmail.cim>
2024-03-18 12:22:27 -0600

    ComponentDidMount and Django Sessions

--- app/frontend/src/components/HomePage.tsx
+++ app/frontend/src/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
     BrowserRouter as Router,
     Switch,
@@ -6,6 +6,7 @@ import {
     Link,
     Redirect
 } from "react-router-dom";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core"
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
 import Room from './Room'
@@ -15,21 +16,74 @@ interface Props {
 }
 
 interface State {
-
+    roomCode: string
 }
 
 
 export default class HomePage extends React.Component<Props, State> {
+
     constructor(props: Props) {
         super(props);
+        this.state = {
+            roomCode: ''
+        }
+    }
+
+    async componentDidMount() {
+        fetch('/api/user-in-room')
+            .then((response) => response.json())
+            .then((data: { code: string }) => {
+                this.setState({
+                    roomCode: data.code
+                })
+            })
+    }
+
+    renderHomePage() {
+        return (
+            <Grid container spacing={3}>
+                <Grid
+                    xs={12}
+                    item
+                    container
+                    alignItems="center"
+                    justifyContent="center"
+                    direction="column">
+                    <Typography variant="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid
+                    xs={12}
+                    item
+                    container
+                    alignItems="center"
+                    justifyContent="center"
+                    direction="column">
+                    <ButtonGroup
+                        disableElevation
+                        variant="contained"
+                        color="primary"
+                    >
+                        <Button component={Link} to="/join" color="primary">
+                            Join a Room
+                        </Button>
+                        <Button component={Link} to="/create" color="secondary">
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
     }
 
     render() {
         return (<Router>
             <Switch>
-                <Route exact path='/'>
-                    <p>This is the home page</p>
-                </Route>
+                <Route exact path='/' render={() => {
+                    return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`} />) : (this.renderHomePage())
+                }} />
+
                 <Route path='/join' component={RoomJoinPage} />
                 <Route path='/create' component={CreateRoomPage} />
                 <Route path='/room/:roomCode' component={Room} />
